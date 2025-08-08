29cace84d32341b8dd2011b31c50386968c75886
ozkan <ozkan.zorlu@gmail.cpm>
2025-08-08 05:17:24 +0300

    pipeline deneme

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Trabzonspor Şampiyondur'),
+            const Text('Trabzonspor Şampiyondur 2011'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
