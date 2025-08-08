ec9698a2b53791da5c7fd15b2135e43da772694e
ozkan <ozkan.zorlu@gmail.cpm>
2025-08-08 19:24:18 +0300

    Jenkisnfile was added.

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Trabzonspor Şampiyondur 444'),
+            const Text('Trabzonspor Şampiyondur 55'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
