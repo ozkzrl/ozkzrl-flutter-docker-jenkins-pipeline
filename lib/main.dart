925c5796e15b9bd7310b1f5782b95d368bfe3da6
ozkzrl <ozkan.zorlu@gmail.com>
2025-08-27 15:42:28 +0300

    Index.html was updated.

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Özkan Zorlu ve ökfmkäsndfäam'),
+            const Text('Leyla vü Mecnun '),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
