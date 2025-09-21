27d2dec8a35de49bd426d9c3e1f3424bae2bb0cd
ozkzrl <ozkan.zorlu@gmail.com>
2025-09-21 14:46:51 +0300

    Main.dart was changced.

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('sublilulumadd d d d d d d d'),
+            const Text('34. hattuşıluli haahhahahahhhahahahahah'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
